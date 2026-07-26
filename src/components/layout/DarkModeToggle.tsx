9a4a3b24d9ebda64ed4c86440b1b46a46b908527
khalidhub7 <louguikhalid7@gmail.com>
2026-07-26 08:52:43 +0100

    fix: correct dark mode tokens

--- src/components/layout/DarkModeToggle.tsx
+++ src/components/layout/DarkModeToggle.tsx
@@ -1,9 +1,7 @@
-
 import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/shadcn/button";
 
 const DarkModeToggle = () => {
-
   const onClickHandler = () => {
     document.documentElement.classList.toggle("dark");
   };
@@ -13,7 +11,7 @@ const DarkModeToggle = () => {
       className="
       relative flex gap-2 cursor-pointer
       rounded-full p-1
-      ring-1 ring-offset-1 ring-brand-ring
+      bg-zinc-200
     "
 
       onClick={onClickHandler}
@@ -21,16 +19,15 @@ const DarkModeToggle = () => {
       <Sun size={20} className="text-yellow-300" />
       <Moon
         size={20}
-        className="
-        text-purple-600 fill-purple-600 stroke-purple-600
-        "
+        className="text-purple-600 fill-purple-600 stroke-purple-600"
       />
       <div
         className="
-      h-4 aspect-square rounded-full bg-blue-600 absolute
-      left-1 dark:inset-x-auto dark:right-1
-      transition-all duration-300
-      "
+        h-5 aspect-square rounded-full absolute
+        bg-olive-50
+        left-1 dark:inset-x-auto dark:right-1
+        transition-all duration-1000
+        "
       ></div>
     </Button>
   );
