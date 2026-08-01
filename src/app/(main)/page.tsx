e845e39c2bb2689c3177b79ac1e1163ce0d2f023
khalidhub7 <louguikhalid7@gmail.com>
2026-08-01 04:20:22 +0100

    refactor: code restructuring

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,20 +1,10 @@
 /* Imports */
-import Image from "next/image";
-import { TextEffect } from "@/components/ui/motion-primitives/text-effect";
-import { MagneticButton } from "@/components/ui/aceternity/magnetic-button";
-import { TypewriterEffectSmooth } from "@/components/ui/aceternity/typewriter-effect";
 
 import { Zap } from "lucide-react";
 import { FAQSection } from "@/components/sections/FAQSection";
+import { HeroSection } from "@/components/sections/HeroSection";
 
 export default function Home() {
-  const words = [
-    { text: "One", className: "" },
-    { text: "Overlay.", className: "text-blue-500 dark:text-blue-400 " },
-    { text: "Total", className: "" },
-    { text: "Control.", className: "text-blue-500 dark:text-blue-400 " },
-  ];
-
   const features = [
     {
       id: 1,
@@ -44,105 +34,7 @@ export default function Home() {
 
   return (
     <>
-      {/* Hero Section */}
-      <section
-        className="
-        grid grid-cols-1 md:grid-cols-2 place-items-center
-        max-w-7xl mx-auto gap-12 md:gap-20
-        px-4 md:px-8 py-8 md:py-12
-        "
-      >
-        {/* Left Content */}
-        <div
-          className="
-          flex flex-col self-start
-          items-center md:items-start gap-8 md:gap-16
-          text-center md:text-left
-          max-w-xl mx-auto md:mx-0
-          "
-        >
-          {/* Should render an h1 */}
-          <TypewriterEffectSmooth words={words} />
-
-          <TextEffect
-            as="p"
-            per="line"
-            segmentWrapperClassName="overflow-hidden block"
-            className="
-            text-lg md:text-xl text-foreground
-            tracking-wider leading-10
-            "
-            variants={{
-              container: {
-                hidden: { opacity: 0 },
-                visible: { opacity: 1, transition: { staggerChildren: 0.2 } },
-              },
-              item: {
-                hidden: { opacity: 0, y: 40 },
-                visible: { opacity: 1, y: 0, transition: { duration: 0.4 } },
-              },
-            }}
-          >
-            {`The simplest way to add beautiful, live-updating TikTok widgets to your streams.
-Alerts, gifts, likes, chat, goals — all in one URL.
-No messy Browser Sources. No restarts.`}
-          </TextEffect>
-
-          {/* CTA Button */}
-          <div
-            className="
-            w-full flex justify-around
-            md:justify-start md:gap-20
-            "
-          >
-            <button
-              className="
-              px-10 py-3
-              text-olive-50 font-playpen tracking-wide
-              bg-linear-to-r from-brand-btn-2 to-brand-btn-5
-              rounded-lg cursor-pointer
-              hover:shadow-brand-btn-shadow-strong hover:-translate-y-1
-              active:scale-90
-              transition-transform duration-300
-              "
-            >
-              Get Started
-            </button>
-
-            <MagneticButton>
-              <button
-                className="
-                px-6 py-3
-                text-olive-50 font-playpen tracking-wide
-                bg-linear-to-l from-brand-btn-3 to-brand-btn-4
-                rounded-lg cursor-pointer
-                active:scale-90 hover:shadow-brand-btn-shadow
-                transition-transform duration-200
-                "
-              >
-                Watch 45-second demo →
-              </button>
-            </MagneticButton>
-          </div>
-        </div>
-
-        {/* Right Content */}
-        <div
-          className="
-          relative w-full h-120 md:h-140
-          "
-        >
-          <Image
-            src="/hero.png"
-            alt="mhdd Tikify"
-            fill
-            priority
-            sizes="(max-width: 1024px) 90vw, 45vw"
-            className=" object-contain drop-shadow-brand-drop-shadow"
-          />
-        </div>
-      </section>
-
+      <HeroSection />
       {/* Why Choose Us Section */}
       <section className="max-w-7xl mx-auto space-y-7">
         {/* section header */}
@@ -168,7 +60,7 @@ No messy Browser Sources. No restarts.`}
           ))}
         </ul>
       </section>
-      
+
       {/* How It Works Section (todo later) */}
       <section></section>
       {/* FAQ Section */}
