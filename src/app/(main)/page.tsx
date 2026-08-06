f9169a2f887ea3d107a60d942c860d14b074248d
khalidhub7 <louguikhalid7@gmail.com>
2026-08-06 17:51:47 +0100

    fix: fix tailwind tokens

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,6 +4,7 @@ import { HowItWorksSection } from "@/components/sections/HowItWorksSection";
 import { WhyChooseUsSection } from "@/components/sections/WhyChooseUsSection";
 
 const Home = () => {
+  /* main tag content */
   return (
     <div className="max-w-[90%] mx-auto">
       <HeroSection />
