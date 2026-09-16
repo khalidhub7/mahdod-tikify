87bd84eb23a3a8e81c66d75cacd396179f9eeeb3
khalidhub7 <louguikhalid7@gmail.com>
2026-09-16 17:33:05 +0100

    refactor

--- src/components/layout/MobileMenu.tsx
+++ src/components/layout/MobileMenu.tsx
@@ -1,40 +1,32 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import { motion } from "motion/react";
 import { Sheet } from "@/components/ui/shadcn/sheet";
 import { Button } from "@/components/ui/shadcn/button";
 import { SheetTrigger } from "@/components/ui/shadcn/sheet";
 import { LogIn, UserPlus, ChevronRight } from "lucide-react";
 import { House, LayoutDashboard, PanelLeftOpen } from "lucide-react";
+import { AnimatePresence, motion, type Variants } from "motion/react";
 import { SheetClose, SheetContent } from "@/components/ui/shadcn/sheet";
 
 const navLinks = [
-  { label: "Home", href: "/", icon: <House className="size-4" /> },
-  {
-    label: "Dashboard",
-    href: "/dashboard",
-    icon: <LayoutDashboard className="size-4" />,
-  },
-  { label: "Login", href: "/login", icon: <LogIn className="size-4" /> },
-  {
-    label: "Register",
-    href: "/register",
-    icon: <UserPlus className="size-4" />,
-  },
+  { label: "Home", href: "/", icon: House },
+  { label: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
+  { label: "Login", href: "/login", icon: LogIn },
+  { label: "Register", href: "/register", icon: UserPlus },
 ];
 
-// Exit animation isn't used because Sheet handles unmounting internally.
+// exit animation isn't used because Sheet handles unmounting internally.
 
-const listVariants = {
-  open: {
-    transition: { staggerChildren: 0.2, delayChildren: 0.3 },
-  },
+const listVariants: Variants = {
+  open: { transition: { staggerChildren: 0.2, delayChildren: 0.3 } },
   closed: { transition: { staggerChildren: 0.1, staggerDirection: -1 } },
+  exit: { opacity: 0 },
 };
 const listItemVariants = {
   open: { opacity: 1, x: 0 },
   closed: { opacity: 0, x: 10 },
+  exit: { opacity: 0, x: 10 },
 };
 
 // AnimatePresence
@@ -42,17 +34,17 @@ const MobileMenu = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div>
+    <AnimatePresence>
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger
           render={
             <Button
               size={"lg"}
               variant={"outline"}
+              aria-label="Open menu"
               className="
               ring-4 ring-brand-ring ring-offset-2 ring-offset-background
-              size-8 cursor-pointer rounded-full
-              hover:text-purple-600
+              size-8 cursor-pointer rounded-full hover:text-purple-600
               "
             />
           }
@@ -76,43 +68,49 @@ const MobileMenu = () => {
             <motion.ul
               variants={listVariants}
               className="flex flex-col gap-5"
+
               initial="closed"
-              animate={open ? "open" : "closed"}
+              animate="open"
+              exit="exit"
             >
-              {navLinks.map((l) => (
-                <motion.li
-                  key={l.href}
-                  variants={listItemVariants}
-                  whileHover={{ scale: 0.9 }}
-                >
-                  <SheetClose
-                    render={
-                      <Link
-                        href={l.href}
-                        className="
+              {navLinks.map((l) => {
+                const Icon = l.icon;
+
+                return (
+                  <motion.li
+                    key={l.href}
+                    variants={listItemVariants}
+                    whileHover={{ scale: 0.9 }}
+                  >
+                    <SheetClose
+                      render={
+                        <Link
+                          href={l.href}
+                          className="
                         flex gap-5 items-center
 
                         rounded-full p-3 mx-3 text-base text-foreground
                         ring-2 ring-brand-ring ring-offset-2 ring-offset-background
                         hover:bg-accent
                         "
+                        />
+                      }
+                    >
+                      <Icon className="size-4" /> {l.label}
+                      <ChevronRight
+                        className="ml-auto"
+                        size={17}
+                        aria-hidden="true"
                       />
-                    }
-                  >
-                    {l.icon} {l.label}
-                    <ChevronRight
-                      className="ml-auto"
-                      size={17}
-                      aria-hidden="true"
-                    />
-                  </SheetClose>
-                </motion.li>
-              ))}
+                    </SheetClose>
+                  </motion.li>
+                );
+              })}
             </motion.ul>
           </nav>
         </SheetContent>
       </Sheet>
-    </div>
+    </AnimatePresence>
   );
 };
 
